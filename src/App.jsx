76f720ac5513723184adc1fa0fc63fd386bfdff5
iprijaya-mattel <Izvio.PrijayaMumin@Mattel.com>
2023-06-06 15:13:15 +0700

    Add Component and install chart.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import PlaceContentCenter from './components/PlaceContentCenter';
+import Card from './components/Card';
+
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <PlaceContentCenter>
+      <Card>
+        <Card.Title>Line Chart</Card.Title>
+        <Card.Body>balbkabklabkal</Card.Body>
+        <Card.Footer>skksjskjsksj</Card.Footer>
+      </Card>
+    </PlaceContentCenter>
+  );
 }
