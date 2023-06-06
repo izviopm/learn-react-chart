b6fa7abfa3ff51af85b6fcd68bb99163684d0b42
iprijaya-mattel <Izvio.PrijayaMumin@Mattel.com>
2023-06-06 15:25:53 +0700

    Another Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import PlaceContentCenter from './components/PlaceContentCenter';
 import Card from './components/Card';
+import BarChart from './components/BarChart';
+import { useState } from 'react';
 
 export default function App() {
+  const [userdata, setUserData] = useState()
   return (
     <PlaceContentCenter>
       <Card>
         <Card.Title>Line Chart</Card.Title>
-        <Card.Body>balbkabklabkal</Card.Body>
+        <Card.Body>
+          <BarChart chartData={} />
+        </Card.Body>
         <Card.Footer>skksjskjsksj</Card.Footer>
       </Card>
     </PlaceContentCenter>
---
+++ src/components/BarChart.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+export default function BarChart({chartData}) {
+  return <Bar data={chartData} options={} />;
+}
