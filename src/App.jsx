a782cdf08195428111078c48dd24e6a8ffdb1f1a
iprijaya-mattel <Izvio.PrijayaMumin@Mattel.com>
2023-06-06 15:32:24 +0700

    Fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import PlaceContentCenter from './components/PlaceContentCenter';
 import Card from './components/Card';
-import BarChart from './components/BarChart';
-import { useState } from 'react';
 
 export default function App() {
-  const [userdata, setUserData] = useState()
   return (
     <PlaceContentCenter>
       <Card>
         <Card.Title>Line Chart</Card.Title>
-        <Card.Body>
-          <BarChart chartData={} />
-        </Card.Body>
+        <Card.Body>blabkalbalk</Card.Body>
         <Card.Footer>skksjskjsksj</Card.Footer>
       </Card>
     </PlaceContentCenter>
