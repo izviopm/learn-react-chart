26944ac8096aab7480c4a56168d1dd8d1a9dc480
iprijaya-mattel <Izvio.PrijayaMumin@Mattel.com>
2023-06-07 09:24:16 +0700

    Bar Chart.js Success Get Data from Coins API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import PlaceContentCenter from './components/PlaceContentCenter';
 import Card from './components/Card';
+import BarChart from './components/BarChart';
 
 export default function App() {
   return (
     <PlaceContentCenter>
       <Card>
-        <Card.Title>Line Chart</Card.Title>
-        <Card.Body>blabkalbalk</Card.Body>
-        <Card.Footer>skksjskjsksj</Card.Footer>
+        <Card.Title>Bar Chart</Card.Title>
+        <Card.Body>
+          <BarChart />
+        </Card.Body>
       </Card>
     </PlaceContentCenter>
   );
--- src/components/BarChart.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-
-export default function BarChart({chartData}) {
-  return <Bar data={chartData} options={} />;
-}
