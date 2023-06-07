a32c67d7163c11f710d9a260b4f9a61b6c151a34
iprijaya-mattel <Izvio.PrijayaMumin@Mattel.com>
2023-06-07 11:42:23 +0700

    Faker Line Chart Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,10 @@
 import Card from './components/Card';
 import FakerLineChart from './components/FakerLineChart';
-// import BarChart from './components/BarChart';
-// import DoughnutChart from './components/DoughnutChart';
-import LineChart from './components/LineChart';
-// import PieChart from './components/PieChart';
 import PlaceContentCenter from './components/PlaceContentCenter';
 
 export default function App() {
   return (
     <PlaceContentCenter>
-      {/* <Card>
-        <Card.Title>Bar Chart</Card.Title>
-        <Card.Body>
-          <BarChart />
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Title>Doughnut Chart</Card.Title>
-        <Card.Body>
-          <DoughnutChart />
-        </Card.Body>
-      </Card> */}
-      <Card>
-        <Card.Title>Line Chart</Card.Title>
-        <Card.Body>
-          <LineChart />
-        </Card.Body>
-      </Card>
-      {/* <Card>
-        <Card.Title>Pie Chart</Card.Title>
-        <Card.Body>
-          <PieChart />
-        </Card.Body>
-      </Card> */}
       <Card>
         <Card.Title>Faker Line Chart</Card.Title>
         <Card.Body>
